27ae4511d579d2f38a91a3691b6f64eef099a9a3
Murata397 <213988587+Murata397@users.noreply.github.com>
2025-09-26 15:38:28 +0000

    ヘッダーフッター作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
-  def after_sign_out_path_for
+  def after_sign_out_path_for(resource_or_scope)
     root_path
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user, notice: "ログインありがとうございます。"
+      redirect_to @user, notice: "Thank you for logging in."
     else
-      flash.now[:alert] = "ログインに失敗しました。"
+      flash.now[:alert] = "Login failed."
       render :new
     end
   end
