12035f055aa417610d9ff25f0c3e17682a9151e7
Murata397 <213988587+Murata397@users.noreply.github.com>
2025-09-29 02:04:37 +0000

    maintenance view file作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
-  resources :users, only: [:show, :edit, :update]
+  resources :maintenances, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
 end
