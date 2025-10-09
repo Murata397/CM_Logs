c777ae06a1f50dbdd2d9f34db599cc58f3946773
Ubuntu <ubuntu@ip-172-31-33-65.ap-northeast-1.compute.internal>
2025-10-09 03:29:02 +0000

    maintenance new edit show更新

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -2,14 +2,27 @@ class Maintenance < ApplicationRecord
 
   belongs_to :user
   has_many_attached :images
+  has_one_attached :tool_images
 
   validates :title, presence: true
   validates :maintenance_day, presence: true
   validates :maintenance, presence: true
 
-end
-
-
-def index
-  @maintenances = Maintenance.select(:id, :title, :maintenance_day, :maintenance)
+  def index
+    @maintenances = Maintenance.select(:id, :title, :maintenance_day, :maintenance)
+  end
+  def get_tool_images
+    if tool_images
+      tool_images
+    else
+      ActionController::Base.helpers.asset_path('no_image.jpg')
+    end
+  end
+  def get_images
+    if images.attached?
+      images
+    else
+      ActionController::Base.helpers.asset_path('no_image.jpg')
+    end
+  end
 end
